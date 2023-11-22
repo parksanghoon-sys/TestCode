6ddd762e2433232a714ecb01622fbcf976fc9c9e
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2023-11-22 14:05:09 +0900

    초기 커밋.

---
+++ src/wpf.Ui.DataGrid/wpf.Ui.DataGrid/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace wpf.Ui.DataGrid
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<Test> vvv = new ObservableCollection<Test>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            vvv.Add(new Test() { Value = "T1" });
+            vvv.Add(new Test() { Value = "T2" });
+            vvv.Add(new Test() { Value = "T3" });
+            vvv.Add(new Test() { Value = "T4" });
+            datagrid.DataContext = vvv;
+        }
+    }
+    public class Test
+    {
+        public string? Value { get; set; }
+    }
+}
---
+++ src/wpf.Ui.DataGrid/wpf.Ui.DataGrid/PassiveScrollViewer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows;
+
+namespace wpf.Ui.DataGrid
+{
+    public class PassiveScrollViewer : ScrollViewer
+    {
+        /// <summary>Identifies the <see cref="IsScrollSpillEnabled"/> dependency property.</summary>
+        public static readonly DependencyProperty IsScrollSpillEnabledProperty = DependencyProperty.Register(
+            nameof(IsScrollSpillEnabled),
+            typeof(bool),
+            typeof(PassiveScrollViewer),
+            new PropertyMetadata(true)
+        );
+
+        /// <summary>
+        /// Gets or sets a value indicating whether blocked inner scrolling should be propagated forward.
+        /// </summary>
+        public bool IsScrollSpillEnabled
+        {
+            get { return (bool)GetValue(IsScrollSpillEnabledProperty); }
+            set { SetValue(IsScrollSpillEnabledProperty, value); }
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            if (
+                IsVerticalScrollingDisabled
+                || IsContentSmallerThanViewport
+                || (IsScrollSpillEnabled && HasReachedEndOfScrolling(e))
+            )
+            {
+                return;
+            }
+
+            base.OnMouseWheel(e);
+        }
+
+        private bool IsVerticalScrollingDisabled => VerticalScrollBarVisibility == ScrollBarVisibility.Disabled;
+
+        private bool IsContentSmallerThanViewport => ScrollableHeight <= 0;
+
+        private bool HasReachedEndOfScrolling(MouseWheelEventArgs e)
+        {
+            var isScrollingUp = e.Delta > 0;
+            var isScrollingDown = e.Delta < 0;
+            var isTopOfViewport = VerticalOffset == 0;
+            var isBottomOfViewport = VerticalOffset >= ScrollableHeight;
+
+            return (isScrollingUp && isTopOfViewport) || (isScrollingDown && isBottomOfViewport);
+        }
+    }
+}
