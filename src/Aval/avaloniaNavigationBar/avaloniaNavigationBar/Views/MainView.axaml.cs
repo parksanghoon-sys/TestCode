4667f5bafa26089f8f1a208dd99f901aff8c9f19
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2024-03-13 16:24:51 +0900

    update

---
+++ src/Aval/avaloniaNavigationBar/avaloniaNavigationBar/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace avaloniaNavigationBar.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
