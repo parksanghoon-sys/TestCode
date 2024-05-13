0ed7194a185172707210db26cdbca320fef05f58
DESKTOP-DFB0ECA\shpark <shpark94@soletop.co.kr>
2024-05-13 09:37:37 +0900

    update

---
+++ src/CLI/cliConsoleArgTest/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string extensionToRemove = ".pdb";
+        //string filePath = @"D:\WPF_Test_UI\src\CLI\cliConsoleArgTest\bin\Debug\net6.0";
+        //string[] allFiles = Directory.GetFiles(filePath, "*.*", SearchOption.AllDirectories);
+
+
+        //foreach (string file in allFiles)
+        //{
+        //    if(Path.GetExtension(file).Equals(extensionToRemove, StringComparison.OrdinalIgnoreCase))
+        //    {
+        //        File.Delete(file);
+        //    }
+        //}
+        foreach (string arg in args)
+        {
+            string[] allFiles = Directory.GetFiles(arg, "*.*", SearchOption.AllDirectories);
+            foreach (string file in allFiles)
+            {
+                if (Path.GetExtension(file).Equals(extensionToRemove, StringComparison.OrdinalIgnoreCase))
+                {
+                    File.Delete(file);
+                }
+            }
+        }
+        //Console.ReadKey();
+    }
+}
---
+++ src/CLI/cliTypeObject/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    private static void Send(Dictionary<string, object> data)
+    {
+        foreach (var item in data)
+        {
+
+        }
+    }
+}
