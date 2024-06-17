b681c8b51faabf40b3cf895e6f54c82dc4a4a288
DESKTOP-DFB0ECA\shpark <power93266@naver.com>
2024-06-17 19:08:08 +0900

    test : update

--- src/Lib/PacketSupport/UDP_PacketTest/Program.cs
+++ src/Lib/PacketSupport/UDP_PacketTest/Program.cs
@@ -6,26 +6,108 @@ using BytePacketSupport.BytePacketSupport.Services.CheckSum;
 
 public class BitManipulation
 {
+    public byte[] ParseBitValue<T>(byte[] buffer, int startBit, int endBit, T value, bool isBigEndian = false)
+    {
+        int bitLength = endBit - startBit + 1;
 
+        byte[] valueBytes = GetBytes(value);
+        if (isBigEndian)
+        {
+            Array.Reverse(valueBytes);
+        }
+
+        ulong valueAsInt = 0;
+        for (int i = 0; i < valueBytes.Length; i++)
+        {
+            valueAsInt |= ((ulong)valueBytes[i] << (i * 8));
+        }
+
+        ulong mask = (1UL << bitLength) - 1;
+        valueAsInt &= mask;
+
+        for (int i = 0; i < bitLength; i++)
+        {
+            var bitValue = (valueAsInt >> i) & 1UL; // Ensure bitValue is ulong
+            var byteIndex = (startBit + i) / 8;
+            int bitIndex = (startBit + i) % 8;
+
+            buffer[byteIndex] = (byte)((buffer[byteIndex] & ~(1 << bitIndex)) | ((int)bitValue << bitIndex)); // Cast bitValue to int
+        }
+
+        return buffer;
+    }
+
+    private byte[] GetBytes<T>(T value)
+    {
+        if (value is byte b)
+            return new byte[] { b };
+        if (value is sbyte sb)
+            return new byte[] { unchecked((byte)sb) };
+        if (value is short s)
+            return BitConverter.GetBytes(s);
+        if (value is ushort us)
+            return BitConverter.GetBytes(us);
+        if (value is int i)
+            return BitConverter.GetBytes(i);
+        if (value is uint ui)
+            return BitConverter.GetBytes(ui);
+        if (value is long l)
+            return BitConverter.GetBytes(l);
+        if (value is ulong ul)
+            return BitConverter.GetBytes(ul);
+        if (value is float f)
+            return BitConverter.GetBytes(f);
+        if (value is double d)
+            return BitConverter.GetBytes(d);
+
+        throw new ArgumentException($"Type {typeof(T)} is not supported.");
+    }
     public static void SetBitsInByteArray(byte[] data, int startBit, int bitSize, int value)
     {
-        int byteIndex = startBit / 8;
-        int bitIndex = startBit % 8;
+        //int byteIndex = startBit / 8;
+        //int bitIndex = startBit % 8;
+
+        //// Bitmask to isolate the bits we want to set
+        //int mask = (1 << bitSize) - 1;
+
+        //// Shift the value so that it fits in the designated bit range
+        //value = (value & mask) << bitIndex;
+
+        //// Iterate through the bytes and set the appropriate bits
+        //while (bitSize > 0)
+        //{
+        //    // Number of bits we can write to the current byte
+        //    int bitsInCurrentByte = Math.Min(8 - bitIndex, bitSize);
+
+        //    // Mask to clear the target bits in the current byte
+        //    int byteMask = ((1 << bitsInCurrentByte) - 1) << bitIndex;
+
+        //    // Clear the target bits and set the new value
+        //    data[byteIndex] = (byte)((data[byteIndex] & ~byteMask) | (value & byteMask));
+
+        //    // Update the bit counters and indices
+        //    bitSize -= bitsInCurrentByte;
+        //    value >>= bitsInCurrentByte;
+        //    bitIndex = 0;
+        //    byteIndex++;
+        //}
+        int byteIndex = (data.Length * 8 - startBit - bitSize) / 8;
+        int bitIndex = (startBit + bitSize) % 8;
 
         // Bitmask to isolate the bits we want to set
         int mask = (1 << bitSize) - 1;
 
         // Shift the value so that it fits in the designated bit range
-        value = (value & mask) << bitIndex;
+        value = (value & mask) << (8 - bitIndex - bitSize);
 
         // Iterate through the bytes and set the appropriate bits
         while (bitSize > 0)
         {
             // Number of bits we can write to the current byte
-            int bitsInCurrentByte = Math.Min(8 - bitIndex, bitSize);
+            int bitsInCurrentByte = Math.Min(bitIndex + 1, bitSize);
 
             // Mask to clear the target bits in the current byte
-            int byteMask = ((1 << bitsInCurrentByte) - 1) << bitIndex;
+            int byteMask = ((1 << bitsInCurrentByte) - 1) << (8 - bitIndex - bitsInCurrentByte);
 
             // Clear the target bits and set the new value
             data[byteIndex] = (byte)((data[byteIndex] & ~byteMask) | (value & byteMask));
@@ -33,8 +115,25 @@ public class BitManipulation
             // Update the bit counters and indices
             bitSize -= bitsInCurrentByte;
             value >>= bitsInCurrentByte;
-            bitIndex = 0;
-            byteIndex++;
+            bitIndex = (bitIndex + bitsInCurrentByte) % 8;
+            byteIndex += bitIndex == 0 ? -1 : 0;
+        }
+    }
+    public static void SetBitsInByteArrayLittleEndian(byte[] data, int startBit, int bitSize, int value)
+    {
+        int byteOffset = startBit / 8;  // 시작하는 바이트 위치
+        int bitOffset = startBit % 8;   // 바이트 내에서 시작하는 비트 위치
+
+        // Little Endian에서는 하위 비트부터 채워넣음
+        for (int i = 0; i < bitSize; i++)
+        {
+            int bitIndex = bitOffset + i;
+            int byteIndex = byteOffset + (bitIndex / 8);
+            int bitPosition = bitIndex % 8;
+
+            // value에서 해당 비트를 가져와서 data에 채워넣음
+            bool bitValue = ((value >> i) & 1) == 1;
+            data[byteIndex] = (byte)((data[byteIndex] & ~(1 << (7 - bitPosition))) | ((bitValue ? 1 : 0) << (7 - bitPosition)));
         }
     }
 }
@@ -72,9 +171,19 @@ internal class Program
         model.PPC_Touch = 1;
         model.SPC_Touch = 1;
         model.PSU_Card_4 = 1;
-        BitManipulation.SetBitsInByteArray(model.LRU_BIT_MAIN, 0, 1, (int)model.PPC_Touch);
-        BitManipulation.SetBitsInByteArray(model.LRU_BIT_MAIN, 3, 1, (int)model.SPC_Touch);
-        BitManipulation.SetBitsInByteArray(model.LRU_BIT_MAIN, 60, 1, (int)model.PSU_Card_4);
+        model.AMU_Card_2 = 1;
+        model.SPC_Touch_SW = 1;
+
+        BitManipulation.SetBitsInByteArray(model.LRU_BIT_MAIN, 0, 2, (int)model.PPC_Touch);
+        BitManipulation.SetBitsInByteArray(model.LRU_BIT_MAIN, 2, 2, (int)model.SPC_Touch);
+        BitManipulation.SetBitsInByteArray(model.LRU_BIT_MAIN, 46, 2, (int)model.PSU_Card_4);
+        BitManipulation.SetBitsInByteArray(model.SW_BIT, 1, 1, (int)model.SPC_Touch_SW);
+        BitManipulation.SetBitsInByteArray(model.LRU_BIT_ANTENA, 8, 1, (int)model.AMU_Card_2);
+
+        Array.Reverse(model.LRU_BIT_MAIN);
+        Array.Reverse(model.SW_BIT);
+        Array.Reverse(model.LRU_BIT_ANTENA);
+
         var data = model.SerializeToByteArray();
 
         // 메시지를 전송합니다.
@@ -90,7 +199,7 @@ public class DataModel
 
         //Array.Reverse(BitConverter.GetBytes(DestinationID).ToArray());
         //Array.Reverse(BitConverter.GetBytes(MessageType).ToArray());
-        Timespan = new byte[5] { 0x00, 0x00,0x02,0x00,0x00};
+        Timespan = new byte[5] { 0x00, 0x00, 0x02, 0x00, 0x00 };
         SW_Version_In_PPC = new byte[4];
         SW_Version_In_SPC = new byte[4];
         SW_Version_In_MC = new byte[4];
@@ -103,7 +212,7 @@ public class DataModel
     }
     public byte[] SerializeToByteArray()
     {
-        var packet = new PacketBuilder(new PacketBuilderConfiguration() { DefaultEndian = BytePacketSupport.Enums.EEndian.BIG })            
+        var packet = new PacketBuilder(new PacketBuilderConfiguration() { DefaultEndian = BytePacketSupport.Enums.EEndian.BIG })
             .BeginSection("packet")
             .AppendShort(Sequence)
             .AppendUShort(MessageLength)
@@ -112,6 +221,7 @@ public class DataModel
             .AppendUShort(MessageType)
             .AppendUShort(MessageProperties)
             .AppendBytes(Timespan)
+
             .AppendByte(Total_BIT)
             .AppendByte(GCS_Type)
             .AppendBytes(SW_Version_In_PPC)
@@ -124,47 +234,13 @@ public class DataModel
             .AppendBytes(LRU_BIT_ANTENA)
             .AppendBytes(LRU_BIT_SPVSR)
             .AppendByte(PHONE_BIT)
+
             .EndSection("packet")
             .Compute(CheckSum16Type.NORNAL, false)
             .Build();
         return packet;
-        //using (MemoryStream ms = new MemoryStream())
-        //using (BinaryWriter writer = new BinaryWriter(ms))
-        //{
-        //    writer.Write((short)0);
-        //    writer.Write((short)39);
-        //    writer.Write(0x120b0115);
-        //    writer.Write(DestinationID);
-        //    writer.Write(MessageType);
-        //    writer.Write(MessageType);
-        //    writer.Write(Timespan);
-
-        //    writer.Write(Total_BIT);
-        //    writer.Write(GCS_Type);
-        //    writer.Write(SW_Version_In_PPC);
-        //    writer.Write(SW_Version_In_SPC);
-        //    writer.Write(SW_Version_In_MC);
-        //    writer.Write(SW_Version_In_SPV);
-        //    writer.Write(LRU_BIT_MAIN);
-        //    writer.Write(SW_BIT);
-        //    writer.Write(LRU_BIT_RADIO);
-        //    writer.Write(LRU_BIT_ANTENA);
-        //    writer.Write(LRU_BIT_SPVSR);
-        //    writer.Write((byte)0x00);
-        //    writer.Write(SumUp(ms.ToArray()));
-        //    return ms.ToArray();
-        //}
     }
-    public ushort SumUp(byte[] frame)
-    {
-        ushort num = 0;
-        for (int i = 0; i < frame.Length; i++)
-        {
-            num += frame[i];
-        }
 
-        return num;
-    }
     public short Sequence;
 
     public ushort MessageLength;
@@ -251,5 +327,5 @@ public class DataModel
     public double PCU_Card_5 { get; set; }
     public byte PHONE_BIT { get; set; }
 
-  
+
 }
