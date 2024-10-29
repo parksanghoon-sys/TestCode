ccce7592f96b667bfd697ad3a00f576f16d545e2
DESKTOP-DFB0ECA\shpark <power93266@naver.com>
2024-10-29 16:17:08 +0900

    feat : MinimalAPI 구현
    1. MinimalAPI 구성
    2. Endpoint 확장 클래스로 관리
    3. UnitTest 구성

---
+++ src/Web/MinimalAPI/MinimalApiSample/MinimalApiSample.Api/WeatherForecasts/WeatherForecastEndpoints.cs
@@ -0,0 +1,41 @@
+﻿
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+namespace MinimalApiSample.Api.WeatherForecasts;
+
+public static class WeatherForecastEndpoints
+{
+    public static void AddWeatherForecastEndpoints(this WebApplication app)
+    {
+        app.MapGet("/forecast", GetForecast)
+          .WithName("GetWeatherForecast")
+          .WithOpenApi(x => new OpenApiOperation(x)
+          {
+              Summary = "Fetch the weather forecast for a given location"
+          });
+    }
+
+    public static async Task<Results<Ok<WeatherForecastResult>, ProblemHttpResult>> GetForecast([FromQuery(Name = "latitude")] double latitude,
+        [FromQuery(Name = "longitude")] double longitude,
+        [FromServices] WeatherForecastService weatherForecastService)
+    {
+        var forecaset= await weatherForecastService.GetForecast(latitude, longitude);
+
+        if (latitude < -90 || latitude > 90)
+        {
+            return TypedResults.Problem(
+                "The value for latitude is out of range. Must be between -90 and 90",
+                statusCode: StatusCodes.Status400BadRequest
+            );
+        }
+
+        var result = new WeatherForecastResult()
+        {
+            Time = DateTime.UtcNow,
+            TemperatureC = forecaset.Current.Templerature,
+        };
+        return TypedResults.Ok(result);
+    }
+}
---
+++ src/Web/MinimalAPI/MinimalApiSample/MinimalApiSample.Tests/WeatherForecastTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using MinimalApiSample.Api.WeatherForecasts;
+using Moq;
+using RichardSzalay.MockHttp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinimalApiSample.Tests
+{
+    public class WeatherForecastTests
+    {
+        private string sampleResponse =
+       """
+            {
+              "generationtime_ms": 0.01704692840576172,
+              "utc_offset_seconds": 0,
+              "timezone": "GMT",
+              "timezone_abbreviation": "GMT",
+              "elevation": 38,
+              "current_units": {
+                "time": "iso8601",
+                "interval": "seconds",
+                "temperature_2m": "°C"
+              },
+              "current": {
+                "time": "2024-07-03T09:30",
+                "interval": 900,
+                "temperature_2m": 16.3
+              }
+            }
+        """;
+
+        [Test]
+        public async Task It_return_the_expected_temperature()
+        {
+
+            var latitude = 22.4;
+            var longitude = 23.5;
+
+            var mockHttp = new MockHttpMessageHandler();
+            mockHttp.When(WeatherForecastService.BuildUrl(latitude,longitude).ToString())
+                .Respond("application/json",sampleResponse);
+
+            var clientFactory = new Mock<IHttpClientFactory>();
+
+            clientFactory.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(mockHttp.ToHttpClient());
+
+            var forecast = await WeatherForecastEndpoints.GetForecast(latitude, longitude, new WeatherForecastService(clientFactory.Object));
+
+            Assert.That(forecast.Result, Is.TypeOf<Ok<WeatherForecastResult>>());
+
+            var okResult = (Ok<WeatherForecastResult>)forecast.Result;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(okResult.Value, Is.Not.Null);
+                Assert.That(okResult.Value?.TemperatureC, Is.EqualTo(16.3));
+            });
+        }
+    }
+
+}
