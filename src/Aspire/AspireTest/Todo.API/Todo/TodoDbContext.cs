8e2e8dfeca16cf04887cd38a46b1c79de5ecc829
DESKTOP-DFB0ECA\shpark <power93266@naver.com>
2024-10-30 17:48:47 +0900

    style : 불필요한 using 제거

--- src/Aspire/AspireTest/Todo.API/Todo/TodoDbContext.cs
+++ src/Aspire/AspireTest/Todo.API/Todo/TodoDbContext.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Todo.API.Todo.Model;
 
 namespace Todo.API.Todo
