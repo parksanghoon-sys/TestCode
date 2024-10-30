3719582f07eb0c0af356b6b432634360182ebad0
DESKTOP-DFB0ECA\shpark <power93266@naver.com>
2024-10-30 17:46:54 +0900

    feat : 서브 프로젝트로 분리 진행

--- src/Aspire/AspireTest/Todo.API/Program.cs
+++ src/Aspire/AspireTest/Todo.API/Program.cs
@@ -1,8 +1,4 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.IdentityModel.Tokens;
 using System.Text;
-using Todo.API.Identity;
 using Todo.API.Todo;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,49 +10,17 @@ builder.Services.AddSwaggerGen();
 
 builder.AddServiceDefaults();
 
-builder.AddNpgsqlDataSource("todo");
+builder.AddNpgsqlDataSource("pgdb");
 
 builder.AddNpgsqlDbContext<TodoDbContext>("todo");
 
 //builder.AddMySqlDbContext<TodoDbContext>("todo");
 
-builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
-    .AddEntityFrameworkStores<TodoDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.Configure<IdentityOptions>(options =>
-{
-    // Default password settings
-    options.Password.RequireDigit = false;
-    options.Password.RequireLowercase = false;
-    options.Password.RequireNonAlphanumeric = false;
-    options.Password.RequireUppercase = false;
-    options.Password.RequiredLength = 6;
-    options.Password.RequiredUniqueChars = 1;
-});
 
 
-var key = Encoding.ASCII.GetBytes("f6C04Y75teWYVCTLeaSEeX8CkuQ"); // 보안 키 설정 필요
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(key),
-        ValidateIssuer = false,
-        ValidateAudience = false
-    };
-});
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("/todo", policy =>
-    policy.RequireAuthenticatedUser());
-});
+
+
+
 
 var app = builder.Build();
 
